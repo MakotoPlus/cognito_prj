d9dbddd41754ae288a7cc9e5cd67db84a9ec80b7
makoto <fukupeemail@gmail.com>
2021-05-04 20:57:33 +0900

    初回

---
+++ cognito_prj/cognito_prj/urls.py
@@ -0,0 +1,35 @@
+#from django.contrib.auth import views as auth_views
+#from django.urls import re_path
+
+#from .views import ProfileView,UpdateProfileView,MySubsriptions,\
+#    AdminListUsers,AdminSubscriptions,LogoutView
+
+app_name ='cognito_prj'
+
+
+"""cognito_prj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth.views import LoginView, TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^accounts/', include(('django_warrant.urls','dw'))),
+    path(r'user_info/', TemplateView.as_view(template_name='warrant/user_info.html'), name='user_info'),
+    url(r'', LoginView.as_view(), {'template_name': 'warrant/login.html'}, name='login'),
+]
